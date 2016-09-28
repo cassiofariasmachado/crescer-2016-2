8b60020e19c3e071869efd069c0c4ed9766219ab
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-09-28 01:29:43 -0300

    feat(Dwarf): implementar Dwarves.

---
+++ src/modulo-01-java-OO/modulo-01/Dwarf.java
@@ -0,0 +1,15 @@
+public class Dwarf{
+    public int vida;
+    
+    public Dwarf (){
+        this.vida = 110;
+    }
+    
+    public void setVida(int vida){
+        this.vida = vida;
+    }
+    
+    public int getVida(){
+        return this.vida;
+    }
+}
