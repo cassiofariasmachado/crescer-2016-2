c8574e7fc2c8c38f20adc28d1abf53cfdd0ee161
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-09-28 00:36:25 -0300

    feat(CestoDeLembas): adicionar contrutor e metodo podeDividirEmPares.

--- src/modulo-01-java-OO/modulo-01/CestoDeLembas.java
+++ src/modulo-01-java-OO/modulo-01/CestoDeLembas.java
@@ -1,33 +1,13 @@
-
-/**
- * Escreva a descrição da classe CestoDeLembas aqui.
- * 
- * @author (seu nome) 
- * @version (número de versão ou data)
- */
-public class CestoDeLembas
-{
-    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
-    private int x;
-
-    /**
-     * COnstrutor para objetos da classe CestoDeLembas
-     */
-    public CestoDeLembas()
-    {
-        // inicializa variáveis de instância
-        x = 0;
+public class CestoDeLembas{
+    private int lembas;
+    
+    public CestoDeLembas(int lembas){
+        this.lembas = lembas;
     }
-
-    /**
-     * Exemplo de método - substitua este comentário pelo seu próprio
-     * 
-     * @param  y   exemplo de um parâmetro de método
-     * @return     a soma de x com y 
-     */
-    public int sampleMethod(int y)
-    {
-        // ponha seu código aqui
-        return x + y;
+     
+    public boolean podeDividirEmPares(int quantidade){
+        if (quantidade % 2 == 0 && quantidade > 2)
+            return true;
+        return false;
     }
 }
