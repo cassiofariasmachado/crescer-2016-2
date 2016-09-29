98a7dfad1d9ec41f607a5c335acb2720338c870b
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-09-29 20:11:02 -0300

    feat(Inventario): adicionar getter para variável itens.

--- src/modulo-01-java-OO/modulo-01/Inventario.java
+++ src/modulo-01-java-OO/modulo-01/Inventario.java
@@ -3,6 +3,10 @@ import java.util.ArrayList;
 public class Inventario {
     private ArrayList<Item> itens = new ArrayList<>();
     
+    public ArrayList<Item> getItens(){
+        return this.itens;
+    }
+    
     public void adicionarItem (Item item) {
         itens.add(item);
     }
