efb2c15b91d9ee6664b1ebb8245af329e31d66ab
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-10-01 13:13:51 -0300

    fix(HobbitContador): corrigir erro ao calcular MMC de zero e MDC de números negativos.

--- src/modulo-01-java-OO/modulo-01/HobbitContador.java
+++ src/modulo-01-java-OO/modulo-01/HobbitContador.java
@@ -1,30 +1,41 @@
 import java.util.ArrayList;
 
 public class HobbitContador {
-    
+
     public int calcularDiferenca(ArrayList<ArrayList<Integer>> arrayDePares) {
         int diferenca = 0;
-        for (ArrayList<Integer> parAtual : arrayDePares)
-            diferenca += getProdutoPar(parAtual) - getMMCPar(parAtual); 
+        for (ArrayList<Integer> parAtual : arrayDePares) {
+            diferenca += this.getProdutoPar(parAtual) - this.getMMCPar(parAtual); 
+        }
         return diferenca;
     }
-    
+
     public int getProdutoPar(ArrayList<Integer> par) {
         return par.get(0) * par.get(1);
     }
-    
+
     public int getMMCPar(ArrayList<Integer> par) {
         return this.getMMC(par.get(0),par.get(1));
     }
-    
+
     public int getMMC(int x, int y) {
+        // lcm (x, y) = |x . y| / gcd(x, y)
+        if (x == 0 || y == 0)
+            return 0;
+        // Garantindo que são positivos (módulo)
+        x = Math.abs (x);
+        y = Math.abs (y);
         return x * y / this.getMDC(x, y);
     }
-    
+
     public int getMDC(int x, int y) {
+        // Algoritmo de euclides: 
+        // gcd (x, 0) == x  
+        // gcd (x, y) = gcd (y, x mod y)
         if (y == 0)
             return x;
-        return this.getMDC(y, x % y);
+        // Garante retorno positivo segundo propriedade: mdc(-x,y) = mdc (x,-y) = mdc (-x,-y) = mdc(x,y)
+        return Math.abs(this.getMDC(y, x % y));
     }
 }
 
