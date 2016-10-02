6f7820d82b4cf0c3751ac78f152043ba4823e91f
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-10-02 14:43:19 -0300

    refactor(Elfo): simplificar atirarFlecha() e revisar classe.

--- src/modulo-01-java-OO/modulo-01/Elfo.java
+++ src/modulo-01-java-OO/modulo-01/Elfo.java
@@ -3,73 +3,73 @@ public class Elfo {
     private Inventario inventario;
     private int experiencia;
     private Status status;
-    
-    public Elfo(String n) {
+
+    public Elfo(String nome) {
         // Chamando outro construtor 
-        this(n, 42);
+        this(nome, 42);
     }
-    
+
     public Elfo(String nome, int quantidadeFlechas){
         this.nome = nome;
         this.inventario = new Inventario();
         this.inventario.adicionarItem(new Item("Arco", 1));
         this.inventario.adicionarItem(new Item("Flechas", quantidadeFlechas <= 0 ? 42 : quantidadeFlechas));
-        status = Status.VIVO;
+        this.status = Status.VIVO;
     }
 
-    public void setNome(String n) {
-        nome = n;
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     public String getNome() {
-        return nome;
+        return this.nome;
     }
-    
+
     public Item getArco() {
         return this.inventario.getItens().get(0);
     }
-    
+
     public Item getFlecha(){
         return this.inventario.getItens().get(1);
     }
-    
+
     public int getExperiencia(){
-        return experiencia;
+        return this.experiencia;
     }
-    
+
     public Status getStatus(){
         return this.status;
     }
-    
+
     public Inventario getInventario() {
         return this.inventario;
     }
-    
+
     public void atirarFlecha(Dwarf dwarf){
-        if (getFlecha().getQuantidade() > 0) {
-            getFlecha().setQuantidade(getFlecha().getQuantidade() - 1);
+        int quantidadeFlechas = this.getFlecha().getQuantidade();
+        if (quantidadeFlechas > 0) {
+            this.getFlecha().setQuantidade(quantidadeFlechas - 1);
+            this.experiencia++;
             dwarf.receberFlecha();
-            experiencia++;
         }
     }
-    
+
     public String toString(){
-        boolean flechaNoSingular = getFlecha().getQuantidade() == 1; 
+        boolean flechaNoSingular = this.getFlecha().getQuantidade() == 1; 
         boolean nivelNoSingular = this.experiencia == 1;
-        
+
         return String.format("%s possui %d %s e %d %s de experiência.", 
-        this.nome, 
-        this.getFlecha().getQuantidade(), 
-        flechaNoSingular ? "flecha" : "flechas",
-        this.experiencia,
-        nivelNoSingular ? "nível" : "níveis");
+            this.nome, 
+            this.getFlecha().getQuantidade(), 
+            flechaNoSingular ? "flecha" : "flechas",
+            this.experiencia,
+            nivelNoSingular ? "nível" : "níveis");
         // return this.nome + " possui " + this.flecha.getQuantidade() + " flechas e " + this.experiencia + " níveis de experiência.";
     }
-    
+
     /*public void atirarFlechaRefactory() {
-        experiencia++;
-        flecha.setQuantidade(flecha.getQuantidade()-1);
+    experiencia++;
+    flecha.setQuantidade(flecha.getQuantidade()-1);
     }*/
 }
-
 
