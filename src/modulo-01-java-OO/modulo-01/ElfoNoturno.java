2b16f3ab03522ec12d4d9d3bdfa8f2fa79437caa
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-10-04 02:52:29 -0300

    feat(ElfoNoturno): adicionar personagem ElfoNoturno que perde cinco porcento de vida a cada atirarFlecha().

---
+++ src/modulo-01-java-OO/modulo-01/ElfoNoturno.java
@@ -0,0 +1,30 @@
+public class ElfoNoturno extends Elfo {
+    
+    public ElfoNoturno(String nome) {
+        super(nome);
+    }
+    
+    public void atirarFlecha(Dwarf dwarf){
+        boolean temFlecha = this.getFlecha() != null && this.getFlecha().getQuantidade() > 0;
+        if (temFlecha) {
+            super.atirarFlecha(dwarf);
+            this.vida -= this.vida*0.05; //elfo noturno 5% perde vida
+            this.status = this.vida <= 0. ? status.MORTO : status.VIVO; // elfo noturno morre
+        }
+    }
+    
+    public void ganharExperiencia() {
+        this.experiencia += 3;
+    }
+    
+    // GAMBIARRA CONHECIDA!!! Método utilizado apenas na classe de testes.
+    public void setVida(double vida) {
+        this.vida = vida;
+    }
+    
+    /*
+     * 5.2: Atualmente (antes do patch 5.3, hehe), elfos noturnos não podem morrer, pois
+     * este comportamento ainda não foi definido, a única verificação de morte era feita 
+     * com o dwarf que recebia a flechada.
+     */
+}
---
+++ src/modulo-01-java-OO/modulo-01/ElfoNoturnoTest.java
@@ -0,0 +1,59 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ElfoNoturnoTest {
+    @Test
+    public void elfoVerdeAtiraUmaFlecha() {
+        // Act
+        ElfoNoturno elfoDoTeste = new ElfoNoturno("Alleria");
+        elfoDoTeste.atirarFlecha(new Dwarf("SemCriatividade"));
+        // Assert
+        assertEquals(41, elfoDoTeste.getFlecha().getQuantidade());
+        assertEquals(3, elfoDoTeste.getExperiencia());
+        assertEquals(95., elfoDoTeste.getVida(), .0);
+    }
+    
+    @Test
+    public void elfoNoturnoAtiraDuasFlechas() {
+        // Act
+        ElfoNoturno elfoDoTeste = new ElfoNoturno("Alleria");
+        elfoDoTeste.atirarFlecha(new Dwarf("SemCriatividade"));
+        elfoDoTeste.atirarFlecha(new Dwarf("SemCriatividade"));
+        // Assert
+        assertEquals(40, elfoDoTeste.getFlecha().getQuantidade());
+        assertEquals(6, elfoDoTeste.getExperiencia());
+        assertEquals(90.25, elfoDoTeste.getVida(), .0);
+    }
+
+    @Test
+    public void elfoNoturnoAtiraCincoFlechas() {
+        // Arrange
+        ElfoNoturno elfoDoTeste = new ElfoNoturno("Sylvanas");
+        // Act
+        for (int i = 0; i < 5; i++)
+            elfoDoTeste.atirarFlecha(new Dwarf("SemCriatividade"));
+        // Assert
+        assertEquals(37, elfoDoTeste.getFlecha().getQuantidade());
+        assertEquals(15, elfoDoTeste.getExperiencia());
+        assertEquals(77.37809375, elfoDoTeste.getVida(), .0);
+    }
+    
+    @Test
+    public void elfoNoturnoComVidaNegativaAtira1000FlechasEMorre() {
+        // Arrange
+        ElfoNoturno elfoDoTeste = new ElfoNoturno("Sylvanas");
+        elfoDoTeste.getFlecha().setQuantidade(1000);
+        Dwarf dwarf = new Dwarf("SemCriatividade");
+        // GAMBIARA CONHECIDA!!!
+        elfoDoTeste.setVida(-10);
+        // Act
+        for (int i = 0; i < 1000; i++)
+            elfoDoTeste.atirarFlecha(dwarf);
+        // Assert
+        assertEquals(0, elfoDoTeste.getFlecha().getQuantidade());
+        assertEquals(3000, elfoDoTeste.getExperiencia());
+        assertEquals(Status.MORTO, elfoDoTeste.getStatus());
+    }
+}
