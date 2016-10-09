1756daf0d93196e232aeef5b6523af96d4baee83
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-10-09 18:32:59 -0300

    doc(ElfoNoturno): revisar classe com pequenas correções nos comentários.

--- src/modulo-01-java-OO/modulo-01/ElfoNoturno.java
+++ src/modulo-01-java-OO/modulo-01/ElfoNoturno.java
@@ -11,8 +11,8 @@ public class ElfoNoturno extends Elfo {
     public void atirarFlecha(Dwarf dwarf){
         if (this.status == Status.VIVO && this.getFlecha().getQuantidade() > 0) {
             super.atirarFlecha(dwarf);
-            this.vida *= 0.95; //elfo noturno 5% perde vida
-            this.status = (int)this.vida == 0 ? status.MORTO : status.VIVO; // elfo noturno morre
+            this.vida *= 0.95; // Elfo noturno perde 5% vida
+            this.status = (int)this.vida == 0 ? status.MORTO : status.VIVO; // Se vida igual a zero, elfo morre
         }
     }
     
@@ -21,8 +21,8 @@ public class ElfoNoturno extends Elfo {
     }
     
     /*
-     * 5.2: Atualmente (antes do patch 5.3, hehe), elfos noturnos não podem morrer, pois
-     * este comportamento ainda não foi definido, a única verificação de morte era feita 
+     * 5.2: Atualmente (antes do patch 5.3), elfos noturnos não podiam morrer, pois
+     * este comportamento ainda não tinha sido definido, a única verificação de morte era feita 
      * com o dwarf que recebia a flechada.
      */
 }
