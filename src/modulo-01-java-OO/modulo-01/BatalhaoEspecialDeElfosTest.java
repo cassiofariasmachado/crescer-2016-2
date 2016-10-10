ba884bbee7b371cef2606b27348209de0afc6f7b
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-10-09 22:38:05 -0300

    test(BatalhaoEspecialDeElfosTest): revisar cenários de teste corrigindo alguns nomes e lógicas, e adicionar cenário para verificar se busca por nome retorna o primeiro elfo.

--- src/modulo-01-java-OO/modulo-01/BatalhaoEspecialDeElfosTest.java
+++ src/modulo-01-java-OO/modulo-01/BatalhaoEspecialDeElfosTest.java
@@ -6,51 +6,80 @@ import java.util.*;
 
 public class BatalhaoEspecialDeElfosTest{
     @Test
-    public void retornaExercitoComoArray() {
+    public void retornaExercitoComoArray() throws NaoPodeAlistarException {
+        // Arrange
         BatalhaoEspecialDeElfos exercito = new BatalhaoEspecialDeElfos();
         ElfoVerde elfo = new ElfoVerde("Verdao");
         exercito.alistar(elfo);
+        // Act and Assert
         assertEquals(elfo, exercito.getExercito()[0]);
     }
     
     @Test
-    public void retornaExercitoDeDoisElfosComoArray() {
+    public void retornaExercitoDeDoisElfosComoArray() throws NaoPodeAlistarException {
+        // Arrange
         BatalhaoEspecialDeElfos exercito = new BatalhaoEspecialDeElfos();
         ElfoVerde elfo = new ElfoVerde("Verdao");
         ElfoVerde elfo2 = new ElfoVerde("Azulao");
         exercito.alistar(elfo);
         exercito.alistar(elfo2);
+        // Act
         Elfo[] elfos = exercito.getExercito(); 
-        assertTrue(contem(elfos, elfo)); 
-        assertTrue(contem(elfos, elfo2)); 
+        // Assert
+        assertTrue(contem(elfos, elfo));
+        assertTrue(contem(elfos, elfo2));
     }
     
     @Test
-    public void alistaUmElfoVerdeEBusca() {
+    public void alistaUmElfoVerdeEBusca() throws NaoPodeAlistarException {
+        // Arrange
         BatalhaoEspecialDeElfos exercito = new BatalhaoEspecialDeElfos();
         ElfoVerde elfo = new ElfoVerde("Verdao");
+        // Act and Assert
         exercito.alistar(elfo);
         assertEquals(elfo, exercito.buscar("Verdao"));
     }
     
     @Test
-    public void alistaUmElfoNoturnoEBusca() {
+    public void alistaUmElfoNoturnoEBusca() throws NaoPodeAlistarException {
+        // Arrange
         BatalhaoEspecialDeElfos exercito = new BatalhaoEspecialDeElfos();
         ElfoNoturno elfo = new ElfoNoturno("BlackElfo");
+        // Act and Assert
         exercito.alistar(elfo);
         assertEquals(elfo, exercito.buscar("BlackElfo"));
     }
     
-    @Test
-    public void alistaUmElfoComumEBusca() {
+    @Test(expected=NaoPodeAlistarException.class)
+    public void alistaUmElfoComum() throws NaoPodeAlistarException {
+        // Arrange
         BatalhaoEspecialDeElfos exercito = new BatalhaoEspecialDeElfos();
         Elfo elfo = new Elfo("Elfo sem clã");
+        // Act and Xabu
         exercito.alistar(elfo);
-        assertNull(exercito.buscar("Elfo sem clã"));
     }
     
     @Test
-    public void buscaElfosVivosNoExercito() {
+    public void buscaPorNomeNoExercitoRetornaPrimeiro() throws NaoPodeAlistarException {
+        // Arrange
+        BatalhaoEspecialDeElfos exercito = new BatalhaoEspecialDeElfos();
+        ElfoNoturno elfo1 = new ElfoNoturno("BlackElfo", 90);
+        ElfoNoturno elfo2 = new ElfoNoturno("BlackElfo", 90);
+        ElfoNoturno elfo3 = new ElfoNoturno("BlackElfo", 90);
+        ElfoVerde elfo4 = new ElfoVerde("GreenElfo");
+        ElfoVerde elfo5 = new ElfoVerde("GreenElfo");
+        exercito.alistar(elfo1);
+        exercito.alistar(elfo2);
+        exercito.alistar(elfo3);
+        exercito.alistar(elfo4);
+        exercito.alistar(elfo5);
+        // Act and Assert
+        assertEquals(elfo4, exercito.buscar("GreenElfo"));
+    }
+    
+    @Test
+    public void buscaElfosVivosNoExercito() throws NaoPodeAlistarException {
+        // Arrange
         BatalhaoEspecialDeElfos exercito = new BatalhaoEspecialDeElfos();
         ElfoNoturno elfo1 = new ElfoNoturno("BlackElfo");
         ElfoVerde elfo2 = new ElfoVerde("GreenElfo");
@@ -62,11 +91,13 @@ public class BatalhaoEspecialDeElfosTest{
         exercito.alistar(elfo3);
         exercito.alistar(elfo4);
         exercito.alistar(elfo5);
+        // Act and Assert
         assertTrue(verificaStatusDoExercito(exercito.buscar(Status.VIVO), Status.VIVO));
     }
     
     @Test
-    public void buscaElfosMortosNoExercito() {
+    public void buscaElfosMortosNoExercito() throws NaoPodeAlistarException {
+        // Arrange
         BatalhaoEspecialDeElfos exercito = new BatalhaoEspecialDeElfos();
         ElfoNoturno elfo1 = new ElfoNoturno("BlackElfo", 90);
         ElfoNoturno elfo2 = new ElfoNoturno("BlackElfo", 90);
@@ -83,11 +114,13 @@ public class BatalhaoEspecialDeElfosTest{
         exercito.alistar(elfo3);
         exercito.alistar(elfo4);
         exercito.alistar(elfo5);
+        // Act and Assert
         assertTrue(verificaStatusDoExercito(exercito.buscar(Status.MORTO), Status.MORTO));
     }
     
     @Test
-    public void buscaElfosMortosEmExercitoQueNaoTem() {
+    public void buscaElfosMortosEmExercitoQueNaoTem() throws NaoPodeAlistarException {
+        // Arrange
         BatalhaoEspecialDeElfos exercito = new BatalhaoEspecialDeElfos();
         ElfoNoturno elfo1 = new ElfoNoturno("BlackElfo");
         ElfoNoturno elfo2 = new ElfoNoturno("BlackElfo");
@@ -99,6 +132,7 @@ public class BatalhaoEspecialDeElfosTest{
         exercito.alistar(elfo3);
         exercito.alistar(elfo4);
         exercito.alistar(elfo5);
+        // Act and Assert
         assertNull(exercito.buscar(Status.MORTO));
     }
     
