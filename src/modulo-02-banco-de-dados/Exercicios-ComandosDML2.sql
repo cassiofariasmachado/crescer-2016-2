fe01917a774388959d4f37132e4b0e29e389f9fa
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-10-11 11:20:55 -0300

    feat(ComandosDML2): adicionar exercícios 2 sobre os comandos DML.

---
+++ src/modulo-02-banco-de-dados/Exercicios-ComandosDML2.sql
@@ -0,0 +1,22 @@
+-- Exercício 1 - Comandos SQL
+SELECT NomeEmpregado 
+FROM Empregado
+ORDER BY DataAdmissao;
+
+-- Exercício 2 - Comandos SQL
+SELECT NomeEmpregado as Nome, 
+	   Salario * 12 as SalarioAnual
+FROM Empregado
+WHERE Cargo = 'Atendente' OR 
+	  SalarioAnual < 18000.00;
+	  
+-- Exercício 3 - Comandos SQL
+SELECT IDCidade
+FROM Cidade
+WHERE Nome = 'Uberlândia';
+
+-- Exercício 4 - Comandos SQL
+SELECT Nome, 
+	   IDCidade
+FROM Cidade
+WHERE UF = 'RS';
