32d7a33d0e26f4ae65855190d8f1d811059f2690
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-10-14 14:53:05 -0300

    fix(Exercicios-FuncoesAgrupamento): corrigir exercícios.

--- src/modulo-02-banco-de-dados/Exercicios-FuncoesAgrupamento.sql
+++ src/modulo-02-banco-de-dados/Exercicios-FuncoesAgrupamento.sql
@@ -1,11 +1,16 @@
 -- Exercicio 1
-SELECT NomeEmpregado, DATEDIFF( MONTH, DataAdmissao, convert(datetime, '31/12/2000', 103) ) AS MesesTrabalhadosAte2000
-FROM Empregado 
-WHERE DataAdmissao BETWEEN convert(datetime, '01/05/1980', 103) AND convert(datetime, '20/01/1982', 103);
+SELECT NomeEmpregado,
+	   DATEDIFF( MONTH, DataAdmissao, convert(datetime, '31/12/2000', 103) ) AS MesesTrabalhadosAte2000
+FROM   Empregado 
+WHERE  DataAdmissao BETWEEN convert(datetime, '01/05/1980', 103) AND 
+	   convert(datetime, '20/01/1982', 103);
 
 -- Exercicio 2
-SELECT MAX(Cargo)
-FROM Empregado
+SELECT TOP 1 WITH TIES Cargo, 
+			  COUNT(*) AS Quantidade
+FROM   Empregado
+GROUP BY Cargo
+ORDER BY Quantidade DESC;
 
 -- Exercicio 3
 SELECT UF, 
@@ -20,17 +25,15 @@ GROUP BY Nome, UF
 HAVING COUNT(*) > 1;
 
 -- Exercicio 5
-SELECT MAX(IDAssociado) + 1 AS ProximoIDAssociado
+SELECT ISNULL(MAX(IDAssociado), 0) + 1 AS ProximoIDAssociado
 FROM Associado;
 
 -- Exercicio 6
 SELECT NomeEmpregado,
 	   Salario,
 	   CASE 
-			WHEN Salario < 1164.00     
-				THEN 0   
-			WHEN Salario > 2326.00
-				THEN 27.5
+			WHEN Salario < 1164.00 THEN 0   
+			WHEN Salario > 2326.00 THEN 27.5
 			ELSE 15
 	   END AS PercentualDeImpostoRenda
 FROM Empregado;
