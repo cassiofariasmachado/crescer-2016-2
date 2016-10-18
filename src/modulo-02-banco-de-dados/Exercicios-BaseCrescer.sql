614f9bbbb7ee649d7d70469752d9c77aa7ec563f
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-10-17 23:35:12 -0200

    refactor(Exercicios-BaseCrescer): corrigir alguns nomes de alias e indentação.

--- src/modulo-02-banco-de-dados/Exercicios-BaseCrescer.sql
+++ src/modulo-02-banco-de-dados/Exercicios-BaseCrescer.sql
@@ -2,15 +2,14 @@
 SELECT COUNT(*) 
 FROM   Pedido
 WHERE  DataPedido BETWEEN convert(datetime, '01/09/2016', 103) AND
-				 convert(datetime, '30/09/2016', 103);
+	   convert(datetime, '30/09/2016', 103);
 
 -- Exercico 2
 SELECT Pro.IDProduto,
 	   Pro.Nome,
 	   Pro.Situacao
 FROM   ProdutoMaterial AS ProMat
-INNER JOIN Produto AS Pro
-ON     ProMat.IDProduto = Pro.IDProduto
+INNER JOIN Produto AS Pro ON     ProMat.IDProduto = Pro.IDProduto
 WHERE  ProMat.IDMaterial = 15836;
 
 -- Exercicio 3
@@ -25,20 +24,18 @@ INSERT INTO Produto(Nome, PrecoCusto, PrecoVenda, Situacao)
 	   VALUES ('Galocha Maragato', 35.67, 77.95, 'A');
 
 -- Exercicio 5
-SELECT Prod.IDProduto,
-	   Prod.Nome
-FROM Produto AS Prod
-LEFT JOIN PedidoItem AS ProI
-ON Prod.IDProduto = ProI.IDProduto
-WHERE ProI.IDProduto IS NULL;
+SELECT Pro.IDProduto,
+	   Pro.Nome
+FROM Produto AS Pro
+LEFT JOIN PedidoItem AS ProIte ON Pro.IDProduto = Pro.IDProduto
+WHERE ProIte.IDProduto IS NULL;
 
 -- Exercicio 6
 -- Cidade com mais clientes
 SELECT TOP 1 WITH TIES Cid.UF, 
 	              COUNT(*) AS QuantidadeClientes
 FROM Cidade AS Cid
-INNER JOIN Cliente AS Cli
-ON Cid.IDCidade = Cli.IDCidade
+INNER JOIN Cliente AS Cli ON Cid.IDCidade = Cli.IDCidade
 GROUP BY Cid.UF
 ORDER BY QuantidadeClientes DESC;
 
@@ -46,8 +43,7 @@ ORDER BY QuantidadeClientes DESC;
 SELECT TOP 1 WITH TIES Cid.UF, 
 	              COUNT(*) AS QuantidadeClientes
 FROM Cidade AS Cid
-INNER JOIN Cliente AS Cli
-ON Cid.IDCidade = Cli.IDCidade
+INNER JOIN Cliente AS Cli ON Cid.IDCidade = Cli.IDCidade
 GROUP BY Cid.UF
 ORDER BY QuantidadeClientes;
 
@@ -61,12 +57,11 @@ FROM PedidoItem AS PedIte
 INNER JOIN ( SELECT * 
 			 FROM Pedido 
 			 WHERE DataEntrega BETWEEN convert(datetime, '01/10/2016', 103) AND
-				      convert(datetime, '31/10/2016', 103) ) AS Ped
-ON Ped.IDPedido = PedIte.IDPedido
+				   convert(datetime, '31/10/2016', 103) ) AS Ped ON Ped.IDPedido = PedIte.IDPedido
 INNER JOIN ( SELECT * 
 			 FROM ProdutoMaterial
-			 WHERE IDMaterial IN (14650, 15703, 15836, 16003, 16604, 17226) AND Quantidade IS NOT NULL) AS ProMat
-ON PedIte.IDProduto = ProMat.IDProduto;
+			 WHERE IDMaterial IN (14650, 15703, 15836, 16003, 16604, 17226) AND 
+			       Quantidade IS NOT NULL) AS ProMat ON PedIte.IDProduto = ProMat.IDProduto;
 
 -- C) 
 SELECT Cli.IDCliente,
@@ -80,14 +75,12 @@ FROM PedidoItem AS PedIte
 INNER JOIN ( SELECT * 
 			 FROM Pedido 
 			 WHERE DataEntrega BETWEEN convert(datetime, '01/10/2016', 103) AND
-				      convert(datetime, '31/10/2016', 103) ) AS Ped
-ON Ped.IDPedido = PedIte.IDPedido
+				   convert(datetime, '31/10/2016', 103) ) AS Ped ON Ped.IDPedido = PedIte.IDPedido
 INNER JOIN ( SELECT * 
 			 FROM ProdutoMaterial
-			 WHERE IDMaterial IN (14650, 15703, 15836, 16003, 16604, 17226) AND Quantidade IS NOT NULL ) AS ProMat
-ON PedIte.IDProduto = ProMat.IDProduto
-INNER JOIN Cliente AS Cli
-ON Cli.IDCliente = Ped.IDCliente;
+			 WHERE IDMaterial IN (14650, 15703, 15836, 16003, 16604, 17226) AND 
+				   Quantidade IS NOT NULL ) AS ProMat ON PedIte.IDProduto = ProMat.IDProduto
+INNER JOIN Cliente AS Cli ON Cli.IDCliente = Ped.IDCliente;
 
 -- Exercicio 8
 SELECT Pro.IDProduto,
@@ -97,10 +90,10 @@ WHERE NOT EXISTS( SELECT IDProduto
                   FROM ProdutoMaterial AS ProMat
 			      WHERE ProMat.IDProduto = Pro.IDProduto );
 
--- Exercício 9 
+-- Exercicio 9 
 SELECT TOP 1 WITH TIES SUBSTRING(Nome, 0, CHARINDEX(' ', Nome)) AS PrimeiroNome, 
 	   COUNT(1) AS Quantidade
-FROM Cliente AS Cli
+FROM Cliente
 GROUP BY SUBSTRING(Nome, 0, CHARINDEX(' ', Nome))
 ORDER BY Quantidade DESC;
 
@@ -115,14 +108,13 @@ INNER JOIN ( SELECT IDProduto
 UPDATE Produto
 SET Situacao = 'Q'
 FROM Produto AS Pro
-INNER JOIN PedidoItem AS PedIte
-ON Pro.IDProduto = PedIte.IDProduto
-INNER JOIN ( SELECT IDProduto 
+INNER JOIN PedidoItem AS PedIte ON PedIte.IDProduto = Pro.IDProduto
+INNER JOIN ( SELECT IDProduto
 			 FROM ProdutoMaterial
 			 WHERE IDMaterial NOT IN (14650, 15703, 15836, 16003, 16604, 17226) ) AS ProMat ON ProMat.IDProduto = Pro.IDProduto
 INNER JOIN ( SELECT IDPedido
 		     FROM Pedido
-		     WHERE DATEDIFF( MONTH, DataPedido, GETDATE() ) >= 2 ) AS Ped ON PedIte.IDPedido = Ped.IDPedido;
+		     WHERE DATEDIFF( MONTH, DataPedido, GETDATE() ) >= 2 ) AS Ped ON Ped.IDPedido = PedIte.IDPedido;
 
 
 UPDATE Produto
