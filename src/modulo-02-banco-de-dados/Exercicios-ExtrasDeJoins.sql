2c12046f2ed1e3a2866c5133b3a71d799b3cff25
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-10-21 01:57:22 -0200

    feat(Exercicios-ExtrasDeJoins): adicionar exercícios do 2 ao 4 (TO-DO: revisar exercícios 1 e 2).

--- src/modulo-02-banco-de-dados/Exercicios-ExtrasDeJoins.sql
+++ src/modulo-02-banco-de-dados/Exercicios-ExtrasDeJoins.sql
@@ -18,3 +18,32 @@ INNER JOIN Cidade AS Cid ON Cid.IDCidade = CliPed.IDCidade
 ORDER BY Cid.UF;
 
 -- Exercicio 2
+-- TO-DO: revisar
+SELECT Cli.IDCliente,
+	   Cli.Nome AS Nome_Cliente,
+	   Cid.Nome AS Nome_Cidade,
+	   Cid.UF
+FROM Cliente AS Cli
+INNER JOIN Cidade AS Cid ON Cid.IDCidade = Cli.IDCidade
+WHERE Cid.Nome IN ( SELECT Nome
+					FROM Cidade
+					GROUP BY Nome
+					HAVING COUNT(1) > 1 );
+
+-- Exercicio 3
+SELECT MIN(IDCidade),
+	   Nome
+FROM Cidade
+GROUP BY Nome
+HAVING COUNT(1) > 1;
+
+-- Exericio 4
+SELECT TOP 30 WITH TIES Pro.IDProduto,
+						Pro.Nome,
+						SUM ( Pro.PrecoVenda - Pro.PrecoCusto ) AS Total_Lucro
+FROM Produto AS Pro
+INNER JOIN PedidoItem AS PedIte ON PedIte.IDProduto = Pro.IDProduto
+INNER JOIN Pedido AS Ped ON Ped.IDPedido = PedIte.IDPedido
+WHERE YEAR(Ped.DataPedido) = 2016
+GROUP BY Pro.IDProduto, Pro.Nome
+ORDER BY Total_Lucro DESC;
