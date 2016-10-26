38f3ba85ca79f423d4eab235c076cf70b16c41f1
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-10-26 14:21:16 -0200

    test(heroi): adicionar cenário de teste criando um objeto Herois recebendo um array com apenas um elemento.

--- src/modulo-04-javascript/dia-02/exercicios/specs/heroisSpecs.js
+++ src/modulo-04-javascript/dia-02/exercicios/specs/heroisSpecs.js
@@ -1,6 +1,14 @@
 describe('new Herois()', function() {
 
   it('deve criar um novo objeto Herois recebendo um array com informações dos personagens', function() {
+    var heroi = {id: 8888888, name: 'Homem-aranha', description: 'O aranha'};
+    var herois = new Array(heroi);
+    var resultado = new Herois(herois);
+    expect(resultado.arrayHerois).toEqual(herois);
+    expect(resultado.arrayHerois[0]).toEqual(heroi);
+  });
+
+  it('deve criar um novo objeto Herois recebendo o array do arquivo marve.js', function() {
     var herois = new Herois(marvel);
     expect(herois.arrayHerois).toEqual(marvel);
   });
