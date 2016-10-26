690cde309ebdda2385b599d653b0d41013c3d5dc
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-10-26 16:17:05 -0200

    test(foraDaGuerraCivil): corrigir caso de testes para realmente verificar se personagens retornados não participaram do evento Civil War.

--- src/modulo-04-javascript/dia-02/exercicios/specs/foraDaGuerraCivilSpecs.js
+++ src/modulo-04-javascript/dia-02/exercicios/specs/foraDaGuerraCivilSpecs.js
@@ -1,12 +1,12 @@
 describe('foraDaGuerraCivil()', function() {
 
-  var verificaSeNaoParticiparamDaGuerraCivil = function (herois) {
-    for (let heroi in herois){
-      var items = heroi.events.items;
-      for (let indice in items) {
-        if (items[indice].name === "Civil War")
+  var verificaSeNaoParticiparamDaGuerraCivil = function(herois) {
+    for (let i = 0, len = herois.length; i < len; i++){
+      var items = herois[i].events.items;
+      for (let j = 0, len = items.length; j < len; j++) {
+        if (items[j].name === "Civil War")
           return false;
-    }
+      }
     return true;
     }
   }
@@ -14,7 +14,7 @@ describe('foraDaGuerraCivil()', function() {
   it('deve retornar apenas quem não participou da Guerra Civil', function() {
     var herois = new Herois(marvel);
     var resultado = herois.foraDaGuerraCivil();
-    expect(resultado).toBeTruthy();
+    expect(verificaSeNaoParticiparamDaGuerraCivil(resultado)).toBeTruthy();
   });
 
 });
