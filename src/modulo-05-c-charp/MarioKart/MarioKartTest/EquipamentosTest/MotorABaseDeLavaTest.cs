a7f561102bf80236128c6b73b119a3f718c56bd5
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-10-27 18:32:46 -0200

    feat(IEquipamento, MotorABaseDeLava, PneuDeCouroDeDragao): implementar interface equipamento, e equipamentos MotorABaseDeLava e PneuDeCouroDeDragao.

---
+++ src/modulo-05-c-charp/MarioKart/MarioKartTest/EquipamentosTest/MotorABaseDeLavaTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MarioKart.Equipamentos;
+
+namespace MarioKartTest.EquipamentosTest
+{
+    [TestClass]
+    public class MotorABaseDeLavaTest
+    {
+        [TestMethod]
+        public void MotorABaseDeLavaTem3DeBonus()
+        {
+            var motorABaseDeLava = new MotorABaseDeLava();
+            Assert.AreEqual(motorABaseDeLava.Bonus, 3);
+        }
+    }
+}
---
+++ src/modulo-05-c-charp/MarioKart/MarioKartTest/EquipamentosTest/PneuDeCouroDeDragaoTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MarioKart.Equipamentos;
+
+namespace MarioKartTest.EquipamentosTest
+{
+    [TestClass]
+    public class PneuDeCouroDeDragaoTest
+    {
+        [TestMethod]
+        public void PneuDeCouroDeDragaoTem2DeBonus()
+        {
+            var pneuDeCouroDeDragao = new PneuDeCouroDeDragao();
+            Assert.AreEqual(pneuDeCouroDeDragao.Bonus, 2);
+        }
+    }
+}
