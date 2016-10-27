54b56c0d87a5953f7c581da37f9a081f2cf7b52e
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-10-27 17:58:14 -0200

    feat(Corredor, NivelDeHabilidade): implementar corredor que possui um nome obrigatório e um enum representando seu nível de habilidade.

---
+++ src/modulo-05-c-charp/MarioKart/MarioKartTest/CorredorTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MarioKart;
+
+namespace MarioKartTest
+{
+    [TestClass]
+    public class CorredorTest
+    {
+        [TestMethod]
+        public void CorredorNasceComNome()
+        {
+            var corredor = new Corredor("Mario", NivelDeHabilidade.Noob);
+            Assert.AreEqual(corredor.Nome, "Mario");
+        }
+
+        [TestMethod]
+        public void CorredorNasceComNivelDeHabilidade()
+        {
+            var corredor = new Corredor("Mario", NivelDeHabilidade.Mediano);
+            Assert.AreEqual(corredor.NivelDeHabilidade, NivelDeHabilidade.Mediano);
+        }
+
+    }
+}
