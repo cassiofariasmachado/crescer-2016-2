1008caec341895ed6a49f5cc73635bc66c1c67f8
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-10-28 17:35:20 -0200

    feat(KartSkyFusion): implementar tipo de kart Sky Fusion que pode ser um equipamento (TO-DO: Revisar e implementar mais testes).

---
+++ src/modulo-05-c-sharp/MarioKart/MarioKart/Karts/KartSkyFusion.cs
@@ -0,0 +1,42 @@
+﻿using MarioKart.Equipamentos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart.Karts
+{
+    public class KartSkyFusion : Kart, IEquipamento
+    {
+        //TO-DO: Revisar e implementar mais testes
+        public KartSkyFusion(Corredor corredor) : base(corredor)
+        {
+        }
+
+        public int BonusVelocidade
+        {
+            get
+            {
+                int bonusDeVelocidadeSkyFusion = this.Equipamentos.Count + 
+                                                 this.GetBonusHabilidadeCorredor();
+                if (bonusDeVelocidadeSkyFusion > 12)
+                    bonusDeVelocidadeSkyFusion += 5;
+                return bonusDeVelocidadeSkyFusion;
+            }
+        }
+
+        public override int GetBonusHabilidadeCorredor()
+        {
+            int bonusVelocidadeDoCorredorDeSkyFusion = 0;
+            if (this.Corredor.NivelDeHabilidade.Equals(NivelDeHabilidade.Noob))
+                bonusVelocidadeDoCorredorDeSkyFusion = 1;
+            if (this.Corredor.NivelDeHabilidade.Equals(NivelDeHabilidade.Mediano))
+                bonusVelocidadeDoCorredorDeSkyFusion = 2;
+            if (this.Corredor.NivelDeHabilidade.Equals(NivelDeHabilidade.Profissional))
+                bonusVelocidadeDoCorredorDeSkyFusion = 3;
+            return base.GetBonusHabilidadeCorredor() + bonusVelocidadeDoCorredorDeSkyFusion;
+        }
+
+    }
+}
---
+++ src/modulo-05-c-sharp/MarioKart/MarioKartTest/KartsTest/KartSkyFusionTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MarioKart;
+using MarioKart.Karts;
+
+namespace MarioKartTest.KartsTest
+{
+    [TestClass]
+    public class KartSkyFusionTest
+    {
+        //TO-DO: Revisar e implementar mais testes
+        [TestMethod]
+        public void DeveRetornarBonusVelocidadeDoKartSkyFusion()
+        {
+            var corredor = new Corredor("Mario", NivelDeHabilidade.Noob);
+            var kartSkyFusion = new KartSkyFusion(corredor);
+            Assert.AreEqual(4, kartSkyFusion.BonusVelocidade);
+        }
+
+        [TestMethod]
+        public void DeveRetornarVelocidadeDoKartSkyFusion()
+        {
+            var corredor = new Corredor("Mario", NivelDeHabilidade.Noob);
+            var kartSkyFusion = new KartSkyFusion(corredor);
+            Assert.AreEqual(7, kartSkyFusion.Velocidade);
+        }
+
+
+    }
+}
