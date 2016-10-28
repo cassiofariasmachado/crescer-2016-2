f4949313d71035ff0ff6538ad7e5d8da9f5a155e
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-10-28 01:32:43 -0200

    feat(KartLight): implementar o tipo de kart light que recebe um bônus de mais 3 quando corredor é noob e de -1 quando profissional.

---
+++ src/modulo-05-c-sharp/MarioKart/MarioKart/Karts/KartLight.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart.Karts
+{
+    public class KartLight : Kart
+    {
+        public KartLight(Corredor corredor) : base(corredor)
+        {
+        }
+
+        public override int Velocidade
+        {
+            get
+            {
+                return base.Velocidade + this.GetBonusDoKartLight();
+            }
+        }
+
+        private int GetBonusDoKartLight()
+        {
+            // Light: ganha velocidade + 3 se dirigido por um corredor Noob, perde 1 de velocidade se dirigido por um corredor Profissional
+            bool corredorNoob = this.Corredor.NivelDeHabilidade.Equals(NivelDeHabilidade.Noob);
+            bool corredorProfissional = this.Corredor.NivelDeHabilidade.Equals(NivelDeHabilidade.Profissional);
+            if (corredorNoob)
+                return 3;
+            if (corredorProfissional)
+                return -1;
+            return 0;
+        }
+
+    }
+}
---
+++ src/modulo-05-c-sharp/MarioKart/MarioKartTest/KartsTest/KartLightTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MarioKart;
+using MarioKart.Karts;
+
+namespace MarioKartTest.KartsTest
+{
+    [TestClass]
+    public class KartLightTest
+    {
+        [TestMethod]
+        public void KartLightRecebeBonusDeMais3SeNivelDeHabilidadeDoCorredorEhNoob()
+        {
+            var corredor = new Corredor("Mario", NivelDeHabilidade.Noob);
+            var kartLight = new KartLight(corredor);
+            var velocidade = kartLight.Velocidade;
+            Assert.AreEqual(9, kartLight.Velocidade);
+        }
+
+        [TestMethod]
+        public void KartLightRecebeBonusDeMenos1SeNivelDeHabilidadeDoCorredorEhProfissional()
+        {
+            var corredor = new Corredor("Luigi", NivelDeHabilidade.Profissional);
+            var kartLight = new KartLight(corredor);
+            var velocidade = kartLight.Velocidade;
+            Assert.AreEqual(8, kartLight.Velocidade);
+        }
+
+        [TestMethod]
+        public void KartLightNaoRecebeBonusSeNivelDeHabilidadeDoCorredorEhMediano()
+        {
+            var corredor = new Corredor("Luigi", NivelDeHabilidade.Mediano);
+            var kartLight = new KartLight(corredor);
+            var velocidade = kartLight.Velocidade;
+            Assert.AreEqual(8, kartLight.Velocidade);
+        }
+
+    }
+}
