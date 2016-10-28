545839be6fbd505e0bcdfa0aa4c9143f7588de88
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-10-28 10:31:05 -0200

    feat(UltraPack): implementar equipamento que possui outro equipamento e que tem bônus igual o do seu equipamento mais 20% arredondado para cima.

---
+++ src/modulo-05-c-sharp/MarioKart/MarioKart/Equipamentos/UltraPack.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart.Equipamentos
+{
+    public class UltraPack : IEquipamento
+    {
+        public UltraPack(IEquipamento equipamento)
+        {
+            this.Equipamento = equipamento;
+        }
+
+        public int Bonus
+        {
+            get
+            {
+                return (int) Math.Ceiling(Equipamento.Bonus * 1.2);
+            }
+        }
+
+        public IEquipamento Equipamento { get; }
+    }
+}
---
+++ src/modulo-05-c-sharp/MarioKart/MarioKartTest/EquipamentosTest/UltraPackTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MarioKart.Equipamentos;
+
+namespace MarioKartTest.EquipamentosTest
+{
+    [TestClass]
+    public class UltraPackTest
+    {
+        [TestMethod]
+        public void DeveCriarUltraPackComUmEquipamento()
+        {
+            var pneuDeCouroDeDragao = new PneusDeCouroDeDragao();
+            var ultraPack = new UltraPack(pneuDeCouroDeDragao);
+            Assert.AreEqual(pneuDeCouroDeDragao, ultraPack.Equipamento);
+        }
+
+        [TestMethod]
+        public void DeveRetornarBonusDoEquipamentoMaisVintePorCento()
+        {
+            var pneuDeCouroDeDragao = new PneusDeCouroDeDragao();
+            var ultraPack = new UltraPack(pneuDeCouroDeDragao);
+            Assert.AreEqual(3, ultraPack.Bonus);
+        }
+
+        [TestMethod]
+        public void DeveRetornarUltraPackComUltraPack()
+        {
+            var pneuDeCouroDeDragao = new PneusDeCouroDeDragao();
+            var ultraPack = new UltraPack(pneuDeCouroDeDragao);
+            var ultraPackComUltraPack = new UltraPack(ultraPack);
+            Assert.AreEqual(pneuDeCouroDeDragao, ultraPack.Equipamento);
+            Assert.AreEqual(ultraPack, ultraPackComUltraPack.Equipamento);
+            Assert.AreEqual(4, ultraPackComUltraPack.Bonus);
+        }
+    }
+}
