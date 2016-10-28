6b81f9b0f1e0815fb7baf75393d995d3eb465147
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-10-28 02:11:48 -0200

    feat(FogueteDePlutonio): implementar equipamento com nível entre um e cinco, e com bônus de um por nível.

---
+++ src/modulo-05-c-sharp/MarioKart/MarioKart/Equipamentos/FogueteDePlutonio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart.Equipamentos
+{
+    public class FogueteDePlutonio : IEquipamento
+    {
+        public FogueteDePlutonio(int nivel = 2)
+        {
+            bool nivelEntreUmECinco = nivel >= 1 && nivel <= 5;
+            this.Nivel = nivelEntreUmECinco ? nivel : 2;
+        }
+
+        public int Bonus
+        {
+            get
+            {
+                return this.Nivel;
+            }
+        }
+
+        public int Nivel { get; }
+
+    }
+}
---
+++ src/modulo-05-c-sharp/MarioKart/MarioKartTest/EquipamentosTest/FogueteDePlutonioTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MarioKart.Equipamentos;
+
+namespace MarioKartTest.EquipamentosTest
+{
+    [TestClass]
+    public class FogueteDePlutonioTest
+    {
+        [TestMethod]
+        public void DeveCriarFogueteDePlutonioComNivelEntreUmECinco()
+        {
+            var fogueteDePlutonio = new FogueteDePlutonio(5);
+            Assert.AreEqual(5, fogueteDePlutonio.Nivel);
+        }
+
+        [TestMethod]
+        public void DeveCriarFogueteDePlutonioComNivelDoisQuandoNaoInformado()
+        {
+            var fogueteDePlutonio = new FogueteDePlutonio();
+            Assert.AreEqual(2, fogueteDePlutonio.Nivel);
+        }
+
+        [TestMethod]
+        public void DeveCriarFogueteDePlutonioComNivelDoisQuandoNivelInvalido()
+        {
+            var fogueteDePlutonio = new FogueteDePlutonio(10);
+            Assert.AreEqual(2, fogueteDePlutonio.Nivel);
+        }
+
+        [TestMethod]
+        public void DeveRetornarBonusDeUmPorNivel()
+        {
+            var fogueteDePlutonio = new FogueteDePlutonio(5);
+            Assert.AreEqual(5, fogueteDePlutonio.Bonus);
+        }
+
+    }
+}
