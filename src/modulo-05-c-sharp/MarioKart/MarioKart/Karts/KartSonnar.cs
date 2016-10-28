47eb131316bac9b9873896fcc35e9a7fc78bdc2e
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-10-28 00:31:14 -0200

    feat(KartSonnar): implementar o tipo de kart sonnar que recebe um bônus de mais 2 se possuir mais de um equipamento.

---
+++ src/modulo-05-c-sharp/MarioKart/MarioKart/Karts/KartSonnar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart.Karts
+{
+    public class KartSonnar : Kart
+    {
+        public KartSonnar(Corredor corredor) : base(corredor)
+        {
+        }
+
+        public override int Velocidade
+        {
+            get
+            {
+                return base.Velocidade + this.GetBonusDoKartSonnar();
+            }
+        }
+
+        private int GetBonusDoKartSonnar()
+        {
+            // Sonnar: possui + 2 de velocidade se possuir pelo menos 1 equipamento.
+            bool possuiUmOuMaisEquipamentos = this.Equipamentos.Count > 0;
+            int bonusDoKartSonnar = possuiUmOuMaisEquipamentos ? 2 : 0;
+            return bonusDoKartSonnar;
+        }
+
+    }
+}
---
+++ src/modulo-05-c-sharp/MarioKart/MarioKartTest/KartsTest/KartSonnarTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MarioKart;
+using MarioKart.Karts;
+using MarioKart.Equipamentos;
+
+namespace MarioKartTest.KartsTest
+{
+    [TestClass]
+    public class KartSonnarTest
+    {
+        [TestMethod]
+        public void KartSonnarRecebeBonusDe2SePossuirPeloMenosUmEquipamento()
+        {
+            var corredor = new Corredor("Mario", NivelDeHabilidade.Profissional);
+            var kartSonnar = new KartSonnar(corredor);
+            var pneusDeCouroDeDragao = new PneusDeCouroDeDragao();
+            kartSonnar.Equipar(pneusDeCouroDeDragao);
+            Assert.AreEqual(14, kartSonnar.Velocidade);
+        }
+
+        [TestMethod]
+        public void KartSonnarNaoRecebeBonusDe2SeNaoPossuirPeloMenosUmEquipamento()
+        {
+            var corredor = new Corredor("Mario", NivelDeHabilidade.Profissional);
+            var kartSonnar = new KartSonnar(corredor);
+            Assert.AreEqual(9, kartSonnar.Velocidade);
+        }
+
+    }
+}
