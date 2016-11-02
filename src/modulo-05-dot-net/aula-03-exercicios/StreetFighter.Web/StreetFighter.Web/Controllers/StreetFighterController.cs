1b2f133d3c64898518353035b848060e0113d91f
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-02 15:28:55 -0200

    refactor(FichaTecnicaModel, FichaTecnica, StreetFighterController): remover atributos da ficha técnica que não são mais necessários.

--- src/modulo-05-dot-net/aula-03-exercicios/StreetFighter.Web/StreetFighter.Web/Controllers/StreetFighterController.cs
+++ src/modulo-05-dot-net/aula-03-exercicios/StreetFighter.Web/StreetFighter.Web/Controllers/StreetFighterController.cs
@@ -20,26 +20,10 @@ namespace StreetFighter.Web.Controllers
         {
             FichaTecnicaModel fichaTecnica = new FichaTecnicaModel();
             fichaTecnica.Nome = "Blanka";
-            fichaTecnica.PrimeiraAparicao = "Street Fighter II The World Warrior(1991)";
             fichaTecnica.DataNascimento = DateTime.Parse("12/02/1966", new CultureInfo("pt-BR"));
             fichaTecnica.Altura = 192;
             fichaTecnica.Peso = 96;
-            fichaTecnica.MedidaBusto = 198;
-            fichaTecnica.MedidaCintura = 120;
-            fichaTecnica.MedidaQuadril = 172;
-            fichaTecnica.TipoSanguineo = TipoSanguineo.B;
-            fichaTecnica.HabilidadesEspeciais = "Caçar, Eletricidade.";
-            fichaTecnica.Gosta = "Frutas tropicais, Pirarucu, Sua mãe.";
-            fichaTecnica.Desgosta = "Army ants(espécie de formiga).";
-            fichaTecnica.EstiloDeLuta = "Luta Selvagem autodidata (Army Ants) / Capoeira.";
             fichaTecnica.Origem = "Brasil (lugar de nascença é provável como sendo Tailândia).";
-            fichaTecnica.FalaDeVitoria = "\"Ver você em ação é uma piada!\"";
-            fichaTecnica.NickNameDoSSF2 = "\"A selvagem criança da natureza.\"";
-            fichaTecnica.NickNameDoSFA3 = "\"A animal pessoa amazônica.\"";
-            fichaTecnica.NickNameDoSF4 = "\"Guerreiro da selva.\"";
-            fichaTecnica.StageDoSFA3 = "Ramificação do Rio Madeira - pantano, Brasil (ramificação do rio Madeira: talvez possa ser Mato Grosso, ou Tocantins ?).";
-            fichaTecnica.StageDoSF2 = "Bacia do rio Amazonas (Brasil).";
-            fichaTecnica.GolpesEspeciaisFamosos = "Electric Thunder, Rolling Attack.";
             return View(fichaTecnica);
         }
 
--- src/modulo-05-dot-net/aula-03-exercicios/StreetFighter.Web/StreetFighter.Web/Models/FichaTecnicaModel.cs
+++ src/modulo-05-dot-net/aula-03-exercicios/StreetFighter.Web/StreetFighter.Web/Models/FichaTecnicaModel.cs
@@ -8,25 +8,9 @@ namespace StreetFighter.Web.Models
     public class FichaTecnicaModel
     {
         public string Nome { set; get; }
-        public string PrimeiraAparicao { set; get; }
         public DateTime DataNascimento { set; get; }
         public int Altura { set; get; }
         public double Peso { set; get; }
-        public int MedidaBusto { set; get; }
-        public int MedidaCintura { set; get; }
-        public int MedidaQuadril { set; get; }
-        public TipoSanguineo TipoSanguineo { set; get; }
-        public string HabilidadesEspeciais { set; get; }
-        public string Gosta { set; get; }
-        public string Desgosta { set; get; }
-        public string EstiloDeLuta { set; get; }
         public string Origem { set; get; }
-        public string FalaDeVitoria { set; get; }
-        public string NickNameDoSSF2 { set; get; }
-        public string NickNameDoSFA3 { set; get; }
-        public string NickNameDoSF4 { set; get; }
-        public string StageDoSFA3 { set; get; }
-        public string StageDoSF2 { set; get; }
-        public string GolpesEspeciaisFamosos { set; get; }
     }
 }
