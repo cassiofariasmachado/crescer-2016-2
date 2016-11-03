ee81d45bcf1a43d318d38f0624a34da60cf209a8
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-03 21:21:24 -0200

    feat(Personagem): adicionar UrlDaImagem, DataNascimento, Altura, Peso, GolpesEspeciais e PersonagemOculto.

--- src/modulo-05-dot-net/aula-03-exercicios/StreetFighter.Web/StreetFighter.Dominio/Personagem.cs
+++ src/modulo-05-dot-net/aula-03-exercicios/StreetFighter.Web/StreetFighter.Dominio/Personagem.cs
@@ -10,20 +10,53 @@ namespace StreetFighter.Dominio
     {
         public int Id { get; private set; }
 
+        public string UrlDaImagem { get; private set; }
+
         public string Nome { get; private set; }
 
-        public string Origem { get; private set; }
+        public DateTime DataNascimento { get; private set; }
 
-        public Personagem(int id, string nome, string origem)
-            :this(nome, origem)
+        public int Altura { get; private set; }
+
+        public double Peso { get; private set; }
+
+        public string Origem { get; private set; }
+  
+        public string GolpesEspeciais { get; private set; }
+
+        public bool PersonagemOculto { get; private set; }
+
+        public Personagem( int id, 
+                           string nome,
+                           DateTime dataNascimento,
+                           int altura,
+                           double peso,
+                           string origem,
+                           string golpesEspeciais,
+                           string urlDaImagem = "Não informado",
+                           bool personagemOculto = false)
+            :this(nome, dataNascimento, altura, peso, origem, golpesEspeciais, urlDaImagem, personagemOculto)
         {
             this.Id = id;
         }
 
-        public Personagem(string nome, string origem)
+        public Personagem( string nome, 
+                           DateTime dataNascimento,
+                           int altura, 
+                           double peso,
+                           string origem,
+                           string golpesEspeciais,
+                           string urlDaImagem = "Não informado",
+                           bool personagemOculto = false )
         {
+            this.UrlDaImagem = urlDaImagem;
             this.Nome = nome;
+            this.DataNascimento = dataNascimento;
+            this.Altura = altura;
+            this.Peso = peso;
             this.Origem = origem;
+            this.GolpesEspeciais = golpesEspeciais;
+            this.PersonagemOculto = personagemOculto;
         }
     }
 }
