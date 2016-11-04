fd7d647721887486f8e3bb361b6965e2da5dcbb1
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-04 21:11:00 -0200

    refactor(PersonagemRepositorio): remover atributo que guardava lista de personagens e construtores que não são mais necessários em nova lógica de modelagem.

--- src/modulo-05-dot-net/aula-03-exercicios/StreetFighter.Web/StreetFighter.Repositorio/PersonagemRepositorio.cs
+++ src/modulo-05-dot-net/aula-03-exercicios/StreetFighter.Web/StreetFighter.Repositorio/PersonagemRepositorio.cs
@@ -11,18 +11,11 @@ namespace StreetFighter.Repositorio
     public class PersonagemRepositorio : IPersonagemRepositorio
     {
         const string CaminhoArquivo = @"C:\Users\Cassio\Documents\git\crescer-2016-2\src\modulo-05-dot-net\aula-03-exercicios\StreetFighter.Web\StreetFighter.Repositorio\repositorio-personagem.csv";
-        public IList<Personagem> Personagens { get; }
 
-        public PersonagemRepositorio() : this(File.ReadAllLines(CaminhoArquivo).ToList())
-        { }
 
-        public PersonagemRepositorio(IList<string> personagens)
         {
-            this.Personagens = new List<Personagem>();
 
-            foreach (string personagemComoString in personagens)
             {
-                Personagens.Add(new Personagem(personagemComoString));
             }
         }
 
