a8aaeae8210ecb6db7b0bbb2d13b11aa81c1d9bb
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-04 00:24:13 -0200

    feat(PersonagemRepositorio): adicionar construtor que recebe uma lista de personagens como string e outro que lê essa lista de um arquivo csv, e respectivo teste.

--- src/modulo-05-dot-net/aula-03-exercicios/StreetFighter.Web/StreetFighter.Repositorio/PersonagemRepositorio.cs
+++ src/modulo-05-dot-net/aula-03-exercicios/StreetFighter.Web/StreetFighter.Repositorio/PersonagemRepositorio.cs
@@ -1,6 +1,7 @@
 ﻿using StreetFighter.Dominio;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,23 @@ namespace StreetFighter.Repositorio
 {
     public class PersonagemRepositorio : IPersonagemRepositorio
     {
-        public List<Personagem> ListarPersonagens (string filtro)
+        const string CaminhoArquivo = @"C:\Users\Cassio\Documents\git\crescer-2016-2\src\modulo-05-dot-net\aula-03-exercicios\StreetFighter.Web\StreetFighter.Repositorio\repositorio-personagem.csv";
+        public IList<Personagem> Personagens { get; }
+
+        public PersonagemRepositorio() : this(File.ReadAllLines(CaminhoArquivo).ToList())
+        { }
+
+        public PersonagemRepositorio(IList<string> personagens)
+        {
+            this.Personagens = new List<Personagem>();
+
+            foreach (string personagemComoString in personagens)
+            {
+                Personagens.Add(new Personagem(personagemComoString));
+            }
+        }
+
+        public List<Personagem> ListarPersonagens (string filtroNome)
         {
             throw new NotImplementedException();
         }
---
+++ src/modulo-05-dot-net/aula-03-exercicios/StreetFighter.Web/StreetFighter.Testes/Repositorio/PersonagemRepositorioTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using FakeItEasy;
+using StreetFighter.Repositorio;
+using StreetFighter.Dominio;
+
+namespace StreetFighter.Testes.Repositorio
+{
+    [TestClass]
+    public class PersonagemRepositorioTest
+    {
+        [TestMethod]
+        public void CriarPersonagemRepositorioRecebendoUmaListaDeStrings()
+        {
+            List<string> personagens = new List<string>(new string[] 
+                                                                     {
+                                                                        "1;Chun-li;12/12/2012;170;59;China;Spining Bird Kick, Kikouken.;http://www.streetfighter.com.br/upload/editor/20120623181304_27.gif;true",
+                                                                        "2;Akuma;13/01/1992;178;80;Japão;Gouhadouken, Goushoryuken.;http://www.streetfighter.com.br/upload/editor/20131018003126_319.png;true"
+                                                                     }
+                                                       );
+
+            var repositorio = new PersonagemRepositorio(personagens);
+
+            Assert.AreEqual(1, repositorio.Personagens[0].Id);
+            Assert.AreEqual(2, repositorio.Personagens[1].Id);
+        }
+
+        
+    }
+}
