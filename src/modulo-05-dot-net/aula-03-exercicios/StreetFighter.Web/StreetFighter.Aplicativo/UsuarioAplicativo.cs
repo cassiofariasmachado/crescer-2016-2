1f329fe08c24a8caf8adac8abc274b865214ee87
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-06 15:43:04 -0200

    feat(UsuarioAplicativo): adicionar classe UsuarioAplicativo com o serviço de buscar o usuário autenticado no repositório e comparar se sua senha é a mesma, e respectivos testes.

---
+++ src/modulo-05-dot-net/aula-03-exercicios/StreetFighter.Web/StreetFighter.Aplicativo/UsuarioAplicativo.cs
@@ -0,0 +1,44 @@
+﻿using StreetFighter.Dominio;
+using StreetFighter.Infraestrutura;
+using StreetFighter.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreetFighter.Aplicativo
+{
+    public class UsuarioAplicativo
+    {
+        private readonly IUsuarioRepositorio repositorio;
+
+        public UsuarioAplicativo() : this(new UsuarioRepositorio())
+        {
+        }
+
+        internal UsuarioAplicativo(IUsuarioRepositorio repositorio)
+        {
+            this.repositorio = repositorio;
+        }
+
+        public Usuario BuscarUsuarioAutenticado(string login, string senha)
+        {
+            Usuario usuarioEncontrado = repositorio.BuscarUsuario(login);
+
+            if (usuarioEncontrado == null)
+                return null;
+
+            string senhaDeComparacao =
+                ServicoDeCriptografia.ConverterParaMD5($"{usuarioEncontrado.Email}_$_{senha}");
+
+            if (usuarioEncontrado != null && usuarioEncontrado.Senha.Equals(senhaDeComparacao))
+            {
+                return usuarioEncontrado;
+            }
+
+            return null;
+        }
+    }
+
+}
---
+++ src/modulo-05-dot-net/aula-03-exercicios/StreetFighter.Web/StreetFighter.Testes/Aplicativo/UsuarioAplicativoTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StreetFighter.Aplicativo;
+using StreetFighter.Dominio;
+using FakeItEasy;
+
+namespace StreetFighter.Testes.Aplicativo
+{
+    [TestClass]
+    public class UsuarioAplicativoTest
+    {
+        [TestMethod]
+        public void BuscarUsuarioAutenticadoDeveBuscarUsuario()
+        {
+            var repositorioFake = A.Fake<IUsuarioRepositorio>();
+
+            var aplicativo = new UsuarioAplicativo(repositorioFake);
+
+            aplicativo.BuscarUsuarioAutenticado("cassio", "123");
+
+            A.CallTo(() => repositorioFake.BuscarUsuario("cassio"))
+                .MustHaveHappened();
+        }
+    }
+}
