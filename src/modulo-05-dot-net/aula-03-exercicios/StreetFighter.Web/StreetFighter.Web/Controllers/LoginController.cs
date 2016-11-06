495e2055766d5c390cde43a0e5bf6d32b9e38b3b
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-06 15:57:01 -0200

    feat(LoginController): adicionar controller para nova view de login com a action FazerLogin que chama as verificações necessárias para autenticar o usuário.

---
+++ src/modulo-05-dot-net/aula-03-exercicios/StreetFighter.Web/StreetFighter.Web/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using StreetFighter.Aplicativo;
+using StreetFighter.Dominio;
+using StreetFighter.Web.Models;
+using StreetFighter.Web.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StreetFighter.Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult FazerLogin(string usuario, string senha)
+        {
+            UsuarioAplicativo aplicativo = new UsuarioAplicativo();
+            Usuario usuarioAutenticado = aplicativo.BuscarUsuarioAutenticado(usuario, senha);
+
+            if (usuarioAutenticado != null)
+            {
+                ServicoDeAutenticacao.Autenticar(new UsuarioLogadoModel(usuarioAutenticado.Login));
+                return RedirectToAction("Index", "StreetFighter");
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
