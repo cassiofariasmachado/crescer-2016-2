f5a9cded624104f1b1537dd0fabe098b9fbc745d
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-06 15:28:29 -0200

    feat(Usuario): adicionar classe Usuario representando os usuários do sistema, e respectivos testes.

---
+++ src/modulo-05-dot-net/aula-03-exercicios/StreetFighter.Web/StreetFighter.Dominio/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreetFighter.Dominio
+{
+    public class Usuario
+    {
+        public string Nome { get; }
+        public string Email { get; }
+        public string Login { get; }
+        public string Senha { get; }
+
+        public Usuario(string nome, string email, string login, string senha)
+        {
+            this.Nome = nome;
+            this.Email = email;
+            this.Login = login;
+            this.Senha = senha;
+        }
+    }
+}
---
+++ src/modulo-05-dot-net/aula-03-exercicios/StreetFighter.Web/StreetFighter.Testes/Dominio/UsuarioTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StreetFighter.Dominio;
+
+namespace StreetFighter.Testes.Dominio
+{
+    [TestClass]
+    public class UsuarioTest
+    {
+        [TestMethod]
+        public void CriarUsuarioDeveCriarUsuarioComNomeEmailLoginESenha()
+        {
+            string nome = "joao paulo";
+            string email = "joaopaulo@gmail.com";
+            string login = "joaop";
+            string senha = "b8cb9d3b6eab36d4bee7df01952652e2";
+
+            Usuario usuario = new Usuario(nome, email, login, senha);
+
+            Assert.AreEqual(nome, usuario.Nome);
+            Assert.AreEqual(email, usuario.Email);
+            Assert.AreEqual(login, usuario.Login);
+            Assert.AreEqual(senha, usuario.Senha);
+        }
+    }
+}
