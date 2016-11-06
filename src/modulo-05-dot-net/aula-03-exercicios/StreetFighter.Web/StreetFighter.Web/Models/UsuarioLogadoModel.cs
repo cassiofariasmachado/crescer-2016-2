f3fd25581a25d6bfce5f04730dc5209fb4de6b0f
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-06 15:58:41 -0200

    feat(UsuarioLogadoModel): adicionar model para o usuário logado que guarda apenas seu login.

---
+++ src/modulo-05-dot-net/aula-03-exercicios/StreetFighter.Web/StreetFighter.Web/Models/UsuarioLogadoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StreetFighter.Web.Models
+{
+    public class UsuarioLogadoModel
+    {
+        public string Login { get; }
+
+        public UsuarioLogadoModel(string login)
+        {
+            this.Login = login;
+        }
+    }
+}
