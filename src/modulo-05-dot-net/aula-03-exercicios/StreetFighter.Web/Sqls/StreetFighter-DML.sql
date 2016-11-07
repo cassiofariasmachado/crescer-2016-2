41b3e6059882180c9b363d165007bd00cf21d7c1
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-07 17:55:49 -0200

    feat(StreetFighter-DDL, StreetFighter-DML): adicionar scritps de DML e DDL utilizados para criar base de dados do projeto.

---
+++ src/modulo-05-dot-net/aula-03-exercicios/StreetFighter.Web/Sqls/StreetFighter-DML.sql
@@ -0,0 +1,13 @@
+INSERT INTO Personagem (Nome, DataNascimento, Altura, Peso, Origem, GolpesEspeciais, UrlDaImagem, PersonagemOculto) 
+			VALUES ('Chun-li', CONVERT(DATETIME, '12/12/2012', 103), 170, 59, 'China', 'Spining Bird Kick, Kikouken.','http://www.streetfighter.com.br/upload/editor/20120623181304_27.gif', 0);
+INSERT INTO Personagem (Nome, DataNascimento, Altura, Peso, Origem, GolpesEspeciais, UrlDaImagem, PersonagemOculto) 
+			VALUES ('Akuma', CONVERT(DATETIME, '13/01/1992', 103), 178, 80, 'Japao', 'Gouhadouken, Goushoryuken.', 'http://www.streetfighter.com.br/upload/editor/20131018003126_319.png', 0);
+INSERT INTO Personagem (Nome, DataNascimento, Altura, Peso, Origem, GolpesEspeciais, UrlDaImagem, PersonagemOculto) 
+			VALUES ('Sagat', CONVERT(DATETIME, '17/02/1991', 103), 189, 89, 'Africa do Sul', 'Tiger Genocide.', 'http://www.streetfighter.com.br/upload/editor/20131104191626_170.png', 1);
+INSERT INTO Personagem (Nome, DataNascimento, Altura, Peso, Origem, GolpesEspeciais, UrlDaImagem, PersonagemOculto) 
+			VALUES ('Balrog', CONVERT(DATETIME, '04/09/1968', 103), 198, 102, 'Estados Unidos', 'Turn Punch, Dash Straight, Dash Uppercut.', 'http://www.streetfighter.com.br/upload/editor/20131018003240_912.png', 1);
+INSERT INTO Personagem (Nome, DataNascimento, Altura, Peso, Origem, GolpesEspeciais, UrlDaImagem, PersonagemOculto) 
+			VALUES ('Blanka', CONVERT(DATETIME, '12/02/1966', 103), 192, 96, 'Brasil', 'Electric Thunder, Rolling Attack.', 'http://www.streetfighter.com.br/upload/editor/20120823194105_127.png', 0);
+
+
+
