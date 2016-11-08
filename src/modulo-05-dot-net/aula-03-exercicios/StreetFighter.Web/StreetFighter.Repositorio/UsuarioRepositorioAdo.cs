7b9b63d034f0430c139025a0dee52e9b8027442a
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-08 00:39:03 -0200

    feat(UsuarioRepositorioAdo): adicionar classe para servir de repositório de usuário utilizando o banco de dados SQL Server.

---
+++ src/modulo-05-dot-net/aula-03-exercicios/StreetFighter.Web/StreetFighter.Repositorio/UsuarioRepositorioAdo.cs
@@ -0,0 +1,49 @@
+﻿using StreetFighter.Dominio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StreetFighter.Dominio;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace StreetFighter.Repositorio
+{
+    public class UsuarioRepositorioAdo : IUsuarioRepositorio
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["StreetFighterConnection"].ConnectionString;
+        public Usuario BuscarUsuario(string login)
+        {
+            Usuario usuario = null;
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = $"SELECT TOP(1) * FROM Usuario WHERE Login = @param_login";
+
+                var command = new SqlCommand(sql, connection);
+                command.Parameters.Add(new SqlParameter("param_login", login));
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    usuario = this.ConverterReaderParaUsuario(reader);
+                }
+
+                connection.Close();
+            }
+            return usuario;
+        }
+
+        private Usuario ConverterReaderParaUsuario(SqlDataReader reader)
+        {
+            return new Usuario( nome: reader["Nome"].ToString(),
+                                email: reader["Email"].ToString(),
+                                login: reader["Login"].ToString(),
+                                senha: reader["Senha"].ToString() );
+        }
+    }
+}
