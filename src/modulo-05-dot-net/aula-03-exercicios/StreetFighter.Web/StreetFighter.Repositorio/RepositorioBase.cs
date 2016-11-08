2258363006b02181c0d31fc774a57d7519404c5b
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-08 17:37:40 -0200

    feat(RepositorioBase): adicionar classe RepositorioBase com o atributo ConnectionString.

---
+++ src/modulo-05-dot-net/aula-03-exercicios/StreetFighter.Web/StreetFighter.Repositorio/RepositorioBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreetFighter.Repositorio
+{
+    public abstract class RepositorioBase
+    {
+        protected string ConnectionString
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings["StreetFighterConnection"].ConnectionString;
+            }
+        }
+    }
+}
