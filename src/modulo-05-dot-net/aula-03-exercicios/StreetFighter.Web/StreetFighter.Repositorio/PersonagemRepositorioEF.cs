70a04fafce6772d09b81021bf3afadc1598f687e
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-08 17:41:44 -0200

    feat(PersonagemRepositorioEF): adicionar um novo repositório de personagens utilizando EF com os métodos BuscarPersonagemPorId() e ListarPersonagens().

---
+++ src/modulo-05-dot-net/aula-03-exercicios/StreetFighter.Web/StreetFighter.Repositorio/PersonagemRepositorioEF.cs
@@ -0,0 +1,46 @@
+﻿using StreetFighter.Dominio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StreetFighter.Dominio;
+
+namespace StreetFighter.Repositorio
+{
+    public class PersonagemRepositorioEF : IPersonagemRepositorio
+    {
+        public Personagem BuscarPersonagemPorId(int id)
+        {
+            using (var contexto = new DatabaseContext())
+            {
+                Personagem personagem = contexto.Personagem.Find(id);
+                return personagem;
+            }
+        }
+
+        public void EditarPersonagem(Personagem personagem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExcluirPersonagem(Personagem personagem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IncluirPersonagem(Personagem personagem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Personagem> ListarPersonagens(string filtroNome)
+        {
+            using (var contexto = new DatabaseContext())
+            {
+                List<Personagem> personagens = contexto.Personagem.Where(personagem => personagem.Nome.Contains(filtroNome)).ToList();
+                return personagens;
+            }
+        }
+    }
+}
