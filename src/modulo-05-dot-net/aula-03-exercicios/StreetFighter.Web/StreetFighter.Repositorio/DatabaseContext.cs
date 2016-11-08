6777afb106bb7d196157fec978088f57e0e5a621
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-08 17:36:33 -0200

    feat(DatabaseContext): adicionar classe DatabaseContext.

---
+++ src/modulo-05-dot-net/aula-03-exercicios/StreetFighter.Web/StreetFighter.Repositorio/DatabaseContext.cs
@@ -0,0 +1,26 @@
+﻿using StreetFighter.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreetFighter.Repositorio
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("StreetFighterConnectionEF")
+        { }
+
+        public DbSet<Personagem> Personagem { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
