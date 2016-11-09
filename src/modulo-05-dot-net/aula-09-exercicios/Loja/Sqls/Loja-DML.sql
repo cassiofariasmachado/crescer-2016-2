f4bf8a5e8b088aa8640dea05fd73e3894d290237
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-09 18:19:17 -0200

    feat(Loja-DML): adicionar scripts de população da base de dados da loja.

---
+++ src/modulo-05-dot-net/aula-09-exercicios/Loja/Sqls/Loja-DML.sql
@@ -0,0 +1,16 @@
+--INSERT INTO Usuario (Email, Senha)
+--	   VALUES('goku@bol.com', 'e99a18c428cb38d5f260853678922e03');
+
+/*INSERT INTO Produto (Nome, Valor)
+	   VALUES ('Omo', 12.99);
+INSERT INTO Produto (Nome, Valor)
+	   VALUES ('Nescau', 5.99);
+INSERT INTO Produto (Nome, Valor)
+	   VALUES ('Ajax', 4.99);
+INSERT INTO Produto (Nome, Valor)
+	   VALUES ('Club Social', 0.99);
+INSERT INTO Produto (Nome, Valor)
+	   VALUES ('Bolacha Recheada', 1.99);
+INSERT INTO Produto (Nome, Valor)
+	   VALUES ('Agua mineral', 2.99);*/
+
