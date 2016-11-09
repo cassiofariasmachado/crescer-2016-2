ea8a66a1354f0d9d11cae7ca16f75ff1ff18ab3c
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-09 00:33:24 -0200

    feat(UsuarioRepositorioEF): adicionar um novo repositório de usuários utilizando EF com o método BuscarUsuario().

---
+++ src/modulo-05-dot-net/aula-03-exercicios/StreetFighter.Web/StreetFighter.Repositorio/UsuarioRepositorioEF.cs
@@ -0,0 +1,21 @@
+﻿using StreetFighter.Dominio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StreetFighter.Dominio;
+
+namespace StreetFighter.Repositorio
+{
+    public class UsuarioRepositorioEF : IUsuarioRepositorio
+    {
+        public Usuario BuscarUsuario(string login)
+        {
+            using (var contexto = new DatabaseContext())
+            {
+                return contexto.Usuario.FirstOrDefault(personagem => personagem.Login.Equals(login));
+            }
+        }
+    }
+}
