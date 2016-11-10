751c013826e9f4b54571e8472d46d33a0d90da91
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-10 00:55:28 -0200

    test(ProdutoServico, ProdutoRepositorioMock): adicionar testes para a classe ProdutoServico.

---
+++ src/modulo-05-dot-net/aula-09-exercicios/Loja/Loja.Tests/Mocks/ProdutoRepositorioMock.cs
@@ -0,0 +1,90 @@
+﻿using Loja.Dominio;
+using Loja.Dominio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja.Tests.Mocks
+{
+    class ProdutoRepositorioMock : IProdutoRepositorio
+    {
+        private IList<Produto> produtos;
+
+        public ProdutoRepositorioMock()
+        {
+            this.produtos = new List<Produto>();
+            this.produtos.Add(new Produto()
+            {
+                Id = 1,
+                Nome = "Nescau",
+                Valor = 5.99M
+            });
+            this.produtos.Add(new Produto()
+            {
+                Id = 2,
+                Nome = "Ajax",
+                Valor = 6.99M
+            });
+            this.produtos.Add(new Produto()
+            {
+                Id = 3,
+                Nome = "Sucrilhos",
+                Valor = 10.33M
+            });
+        }
+
+        private int gerarProximoId()
+        {
+            int maiorId = 0;
+            foreach (var produto in this.produtos)
+            {
+                if (produto.Id > maiorId)
+                    maiorId = produto.Id;
+            }
+            return maiorId + 1;
+        }
+
+        public Produto BuscarProdutoPorId(int id)
+        {
+            return this.produtos.FirstOrDefault(produto => produto.Id == id);
+        }
+
+        public void EditarProduto(Produto produto)
+        {
+            for (int i = 0; i < produtos.Count; i++)
+            {
+                if (produtos[i].Id == produto.Id)
+                {
+                    produtos.RemoveAt(i);
+                    produtos.Insert(i, produto);
+                    break;
+                }
+            }
+        }
+
+        public void ExcluirProduto(Produto produto)
+        {
+            for (int i = 0; i < produtos.Count; i++)
+            {
+                if (produtos[i].Id == produto.Id)
+                {
+                    produtos.RemoveAt(i);
+                    break;
+                }
+            }
+        }
+
+        public void IncluirProduto(Produto produto)
+        {
+            produto.Id = this.gerarProximoId();
+            this.produtos.Add(produto);
+        }
+
+        public IList<Produto> ListarProdutos()
+        {
+            return this.produtos;
+        }
+    }
+}
---
+++ src/modulo-05-dot-net/aula-09-exercicios/Loja/Loja.Tests/ProdutoServicoTest.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Loja.Dominio;
+using Loja.Tests.Mocks;
+using System.Collections.Generic;
+
+namespace Loja.Tests
+{
+    [TestClass]
+    public class ProdutoServicoTest
+    {
+        [TestMethod]
+        public void BuscarProdutoPorIdDeveRetornarProdutoComId1()
+        {
+            ProdutoServico produtoServico = new ProdutoServico(new ProdutoRepositorioMock());
+
+            Produto produto = produtoServico.BuscarProdutoPorId(1);
+
+            Assert.AreEqual(1, produto.Id);
+            Assert.AreEqual("Nescau", produto.Nome);
+            Assert.AreEqual(5.99M, produto.Valor);
+        }
+
+        [TestMethod]
+        public void BuscarProdutoPorIdQueNaoExisteDeveRetornarNull()
+        {
+            ProdutoServico produtoServico = new ProdutoServico(new ProdutoRepositorioMock());
+
+            Produto produto = produtoServico.BuscarProdutoPorId(10);
+
+            Assert.IsNull(produto);
+        }
+
+        [TestMethod]
+        public void SalvarComProdutoComIdDiferenteDe0DeveEditarProduto()
+        {
+            ProdutoServico produtoServico = new ProdutoServico(new ProdutoRepositorioMock());
+
+            Produto produto = new Produto()
+            {
+                Id = 1,
+                Nome = "Toddy",
+                Valor = 4.99M
+            };
+
+            produtoServico.Salvar(produto);
+
+            Produto produtoEditado = produtoServico.BuscarProdutoPorId(1);
+
+            Assert.AreEqual(1, produtoEditado.Id);
+            Assert.AreEqual("Toddy", produtoEditado.Nome);
+            Assert.AreEqual(4.99M, produtoEditado.Valor);
+        }
+
+        [TestMethod]
+        public void SalvarComProdutoComIdIgualAZeroDeveIncluirProduto()
+        {
+            ProdutoServico produtoServico = new ProdutoServico(new ProdutoRepositorioMock());
+
+            Produto produto = new Produto()
+            {
+                Id = 0,
+                Nome = "Toddy",
+                Valor = 5.50M
+            };
+
+            produtoServico.Salvar(produto);
+
+            Produto produtoIncluido = produtoServico.BuscarProdutoPorId(4);
+
+            Assert.AreEqual(4, produtoIncluido.Id);
+            Assert.AreEqual("Toddy", produtoIncluido.Nome);
+            Assert.AreEqual(5.50M, produtoIncluido.Valor);
+        }
+
+        [TestMethod]
+        public void ExcluirDeveRemoverOProduto()
+        {
+            ProdutoServico produtoServico = new ProdutoServico(new ProdutoRepositorioMock());
+
+            Produto produto = produtoServico.BuscarProdutoPorId(2);
+
+            produtoServico.Excluir(produto);
+
+            Produto produtoExcluido = produtoServico.BuscarProdutoPorId(4);
+
+            Assert.IsNull(produtoExcluido);
+        }
+
+        [TestMethod]
+        public void ListarProdutosDeveRetornarTodosOsProdutos()
+        {
+            ProdutoServico produtoServico = new ProdutoServico(new ProdutoRepositorioMock());
+
+            IList<Produto> produtos = produtoServico.ListarProdutos();
+
+            Assert.AreEqual(3, produtos.Count);
+        }
+    }
+}
