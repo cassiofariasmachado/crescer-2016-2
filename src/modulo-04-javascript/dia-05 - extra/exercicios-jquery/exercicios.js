27d7d3db24af3c93be9e15ddba81ef533d66aff9
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-11 10:56:53 -0200

    feat(exercicios): adicionar exercícios extras de jQuery 1C, 1D, 1E, 1F e 1G.

--- src/modulo-04-javascript/dia-05 - extra/exercicios-jquery/exercicios.js
+++ src/modulo-04-javascript/dia-05 - extra/exercicios-jquery/exercicios.js
@@ -1,5 +1,17 @@
 //Exercício 1
 //1A
-$(".module").css("border","solid red 1px");
+$(".module")//.css("border","solid red 1px");
 //1B
-$("#myList li:nth-child(3)").css("color","blue");
+$("#myList li:nth-child(3)")//.css("color","blue");
+//1C
+$("label[for='q']")//.css("color", "yellow");
+//1D
+$("img[alt]")//.css("border","solid red 1px");
+//1E
+$("tr:odd")//.css("background","red");
+//1F
+$("h2:contains('B'):contains('e')")//.css("color", "yellow");
+//1G
+$("#myList:not(.current bar)")//.css("color", "blue");
+//1H
+//Fail - $("img:last").add("h3:last").css("border", "blue solid 10px");
