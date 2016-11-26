3dc940f7922212db5651fac30c40168db59743b7
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-25 23:31:54 -0200

    test(MeuCalendario): adicionar cenários de testes para o método retornarTempoDecorridoAteHoje().

--- src/modulo-09-java/aula1-tema/src/test/java/br/com/cwi/crescer/aula1/tema/MeuCalendarioUtilTest.java
+++ src/modulo-09-java/aula1-tema/src/test/java/br/com/cwi/crescer/aula1/tema/MeuCalendarioUtilTest.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.com.cwi.aula1.tema;
+package br.com.cwi.crescer.aula1.tema;
 
+import java.util.Calendar;
 import java.util.Date;
 import org.junit.Assert;
 import org.junit.Test;
@@ -25,5 +26,25 @@ public class MeuCalendarioUtilTest {
     public void retornarDiaDaSemanaDeveRetornarQuintaFeira() {
         Date data = MeuCalendarioUtil.converterData("24/11/2016");
         Assert.assertEquals("Quinta-feira", MeuCalendarioUtil.retornarDiaDaSemana(data));
+    }
+    
+    @Test
+    public void retornarTempoDecorridoAteHojeDeveRetornarZeroParaDiaMesEAno() {
+        Date data = new Date();
+        Assert.assertEquals("0 ano(s), 0 mes(es) e 0 dia(s)", MeuCalendarioUtil.retornarTempoDecorridoAteHoje(data));
+    }
+    
+    @Test
+    public void retornarTempoDecorridoAteHojeDeveRetornarUmDia() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH, -1);
+        Assert.assertEquals("0 ano(s), 0 mes(es) e 1 dia(s)", MeuCalendarioUtil.retornarTempoDecorridoAteHoje(calendar.getTime()));
+    }
+    
+    @Test
+    public void retornarTempoDecorridoAteHojeDeveRetornarUmAno() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.YEAR, -1);
+        Assert.assertEquals("1 ano(s), 0 mes(es) e 0 dia(s)", MeuCalendarioUtil.retornarTempoDecorridoAteHoje(calendar.getTime()));
     }
 }
