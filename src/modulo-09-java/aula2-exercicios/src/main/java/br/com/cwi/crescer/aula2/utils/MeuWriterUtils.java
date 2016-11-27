f7185cb8bd907ddbf818e4ce83934311ebc6878f
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-27 00:45:20 -0200

    feat(MeuWriterUtils): adicionar classe MeuWriterUtils com método escreverArquivo().

---
+++ src/modulo-09-java/aula2-exercicios/src/main/java/br/com/cwi/crescer/aula2/utils/MeuWriterUtils.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.cwi.crescer.aula2.utils;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author cfari
+ */
+public class MeuWriterUtils {
+
+    public static void main(String[] args) {
+        List<String> conteudo = new ArrayList<>();
+        conteudo.add("Seis");
+        conteudo.add("Sete");
+        conteudo.add("Oito");
+        conteudo.add("Nove");
+        conteudo.add("Dez");
+        escreverArquivo("arquivo.txt", conteudo);
+    }
+
+    public static void escreverArquivo(String arquivo, List<String> conteudo) {
+        try {
+            if (!MeuFileUtils.ehArquivoTxt(arquivo)) {
+                throw new ArquivoInvalidoException();
+            }
+
+            File file = new File(arquivo);
+
+            BufferedWriter buffer = new BufferedWriter(new FileWriter(file, true));
+
+            if (conteudo != null) {
+                for (String linha : conteudo) {
+                    buffer.newLine();
+                    buffer.append(linha);
+                }
+            }
+            
+            buffer.flush();
+        } catch (FileNotFoundException e) {
+            System.err.format("Erro: %s", e.getMessage());
+        } catch (IOException e) {
+            System.err.format("Erro: %s", e.getMessage());
+        } catch (ArquivoInvalidoException e) {
+            System.err.format("Erro: %s", e.getMessage());
+        }
+    }
+}
