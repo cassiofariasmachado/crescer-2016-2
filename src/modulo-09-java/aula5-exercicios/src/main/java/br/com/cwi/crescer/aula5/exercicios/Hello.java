b9969414932fc81cb7e82e7ffea3c92d61e04c9b
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-30 17:40:51 -0200

    feat(aula5-exercicios): adicionar projeto iniciado em aula.

---
+++ src/modulo-09-java/aula5-exercicios/src/main/java/br/com/cwi/crescer/aula5/exercicios/Hello.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.cwi.crescer.aula5.exercicios;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author cassio.machado
+ */
+@ManagedBean
+@ViewScoped
+public class Hello {
+
+    private Pessoa pessoa = new Pessoa();
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+}
---
+++ src/modulo-09-java/aula5-exercicios/src/main/java/br/com/cwi/crescer/aula5/exercicios/Pessoa.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.cwi.crescer.aula5.exercicios;
+
+/**
+ *
+ * @author cassio.machado
+ */
+public class Pessoa {
+
+    private String nome;
+
+    private String sobrenome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    @Override
+    public String toString() {
+        return "O nome da pessoa é " + nome + " e o sobrenome é " + sobrenome;
+    }
+}
