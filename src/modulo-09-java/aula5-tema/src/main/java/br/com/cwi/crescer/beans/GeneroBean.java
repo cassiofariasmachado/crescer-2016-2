951b23b5e806d84a2c15c97f5fb552aeb0e17beb
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-12-01 18:09:05 -0200

    feat(GeneroBean): adicionar bean para genero.

---
+++ src/modulo-09-java/aula5-tema/src/main/java/br/com/cwi/crescer/beans/GeneroBean.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.cwi.crescer.beans;
+
+import br.com.cwi.crescer.daos.AbstractDao;
+import br.com.cwi.crescer.entidades.Genero;
+import java.math.BigInteger;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author cassio.machado
+ */
+@Stateless
+public class GeneroBean extends AbstractDao<Genero, BigInteger> {
+
+    @PersistenceContext(unitName = "crescer")
+    private EntityManager entityManager;
+
+    public GeneroBean() {
+        super(Genero.class);
+    }
+
+    @Override
+    public EntityManager getEntityManager() {
+        return this.entityManager;
+    }
+
+    @Override
+    public List<Genero> findAll() {
+        return this.getEntityManager().createQuery("select g from Genero g").getResultList();
+    }
+}
