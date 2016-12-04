0f8746570afd19e9ba3ad9b1415c78c9f9e10b7d
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-12-04 13:09:27 -0200

    feat(IEntidade): adicionar interface para entidades com métodos getId() e setId().

---
+++ src/modulo-09-java/aula5-tema/src/main/java/br/com/cwi/crescer/entidades/IEntidade.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.cwi.crescer.entidades;
+
+import java.math.BigInteger;
+
+/**
+ *
+ * @author cfari
+ */
+public interface IEntidade {
+
+    public BigInteger getId();
+
+    public void setId(BigInteger id);
+}
